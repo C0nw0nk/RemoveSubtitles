2e376ed64c26684cceb44ec9c9e0aa19949e67df
Conor McKnight <C0nw0nks@googlemail.com>
2024-05-26 16:19:04 +0100

    Update RemoveSubtitles.cmd

--- RemoveSubtitles.cmd
+++ RemoveSubtitles.cmd
@@ -1,7 +1,32 @@
 @echo off & setLocal EnableDelayedExpansion
 
+:: This script will Remove any unwanted subtitle codecs from media files in your library since vobsubs can be buggy and cause transcoding
+
 :: Set the video formats to search for
 set video_formats="-key1 .mkv"
+
+:: These are the subtitle codecs you want to either log or remove from media files
+:: Full List of codecs https://github.com/FFmpeg/FFmpeg/blob/master/libavformat/matroska.c#L67
+:: set sub_codecs="-key1 arib_caption -key2 ass -key3 eia_608 -key4 hdmv_text_subtitle -key5 jacosub -key6 microdvd -key7 mov_text -key8 mpl2 -key9 pjs -key10 realtext -key11 sami -key12 srt -key13 ssa -key14 stl -key15 subrip -key16 subviewer -key17 subviewer1 -key18 text -key19 ttml -key20 vplayer -key21 webvtt -key22 dvb_subtitle -key23 dvb_teletext -key24 dvd_subtitle -key25 hdmv_pgs_subtitle -key26 xsub"
+:: Remove pgs and vob subs only
+set sub_codecs="-key1 hdmv_text_subtitle -key2 dvb_subtitle -key3 dvb_teletext -key4 dvd_subtitle -key5 hdmv_pgs_subtitle"
+
+:: Directory to scan
+:: Path format can be Network share or Drive name
+:: example
+:: set plex_folder="C:\path\Movies"
+:: set plex_folder="\\NAS\FOLDER\Movies"
+set plex_folder=""
+
+:: Remove specified subtitle codecs from media files
+:: 1 is to Remove
+:: 0 is to Keep
+set remove_subtitles_codecs=1
+
+:: Log the output of the codec to a text file the file will be for example dvd_subtitle.txt or ass.txt
+:: 1 is to Remove
+:: 0 is to Keep
+set log_sub_codec_output=1
 
 :: Check for the .PlexCleaner sidecar file to skip already modified media
 :: 1 is to skip
@@ -25,11 +50,28 @@ set looping=0
 
 :: End Edit DO NOT TOUCH ANYTHING BELOW THIS POINT UNLESS YOU KNOW WHAT YOUR DOING!
 
-color 0A
-TITLE C0nw0nk - Plex/Emby media Remove Subtitles
+TITLE C0nw0nk - Plex/Emby Remove Subtitles codecs
 
-echo Input the Directory or Path you want to Remove Subtitles on media items for example C:\path or you can use \\NAS\STORAGE\PATH
+:: Make script configurable via command line with arguements example
+:: "C:\path\RemoveSubtitles.cmd" "\\NAS\path" "sub_codecs" "remove_subtitles_codecs" "log_sub_codec_output" "check_for_sidecar" "pause_window" "wait_interval" "looping" 2^>nul
+:: Working example
+:: "C:\path\RemoveSubtitles.cmd" "\\NAS\path" "-key1 dvb_subtitle -key2 dvb_teletext -key3 dvd_subtitle -key4 hdmv_pgs_subtitle" "1" "1" "0" "1" "0" "0" 2^>nul
+
+if "%~1"=="" goto :script_arguments_not_defined
+set plex_folder="%~1"
+set sub_codecs="%~2"
+set remove_subtitles_codecs=%~3
+set log_sub_codec_output=%~4
+set check_for_sidecar=%~5
+set pause_window=%~6
+set wait_interval=%~7
+set looping=%~8
+:script_arguments_not_defined
+
+if "%plex_folder:"=%"=="" (
+echo Input the Directory or Path you want to Remove Forced Subtitles flags on media items for example C:\path or you can use \\NAS\STORAGE\PATH
 set /p "plex_folder="
+)
 
 set root_path="%~dp0"
 
@@ -40,13 +82,74 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 )
 
 goto :next_download
-
+echo starting removing forced subtitle flags
 :start_exe
 
-:: Remove Subtitle code
+
+:: Forced Subtitle code
 
 set mkvtoolnix_path="%root_path:"=%mkvtoolnix\"
 set "PATH=%PATH%;%mkvtoolnix_path:"=%"
+
+:: set color code
+for /F %%a in ('echo prompt $E ^| cmd') do (
+  set "ESC=%%a"
+)
+:: end color code
+
+:: enum codecs
+set "codec_comd=aws iam create-group %sub_codecs:"=%"
+for /F "tokens=3*" %%p in ("%codec_comd%") do set "codec_tokens=%%q"
+set codec_n=0
+set "key="
+for %%a in (%codec_tokens:-=%) do (
+	if not defined key (
+		set key=%%a
+	) else (
+		set /A codec_n+=1
+		set "codec_token[!codec_n!]=%%a"
+		set "key="
+	)
+)
+:: end enum codecs code
+
+pushd "%plex_folder:"=%" 2>nul
+if errorlevel 1 goto notdir
+goto :isdir
+
+:notdir
+
+		setlocal DisableDelayedExpansion
+		echo Direct file path not a directory "%plex_folder:"=%"
+		for /f "delims=" %%F in ("%plex_folder:"=%") do set "directory_path_name=%%~dpnF"
+
+		for /l %%i in (1,1,%codec_n%) do (
+			echo Enumerating all !codec_token[%%i]! under "%%a"
+			for /f %%b in ('%root_path:"=%win-x64\Tools\FfMpeg\bin\ffmpeg.exe -i "%plex_folder:"=%" 2^>^&1 ^| find /c /i "Subtitle: !codec_token[%%i]!"') do (
+				if [%%b]==[0] (
+					echo !ESC![32m "%%a" does not have !codec_token[%%i]! !ESC![0m
+				) else (
+					echo !ESC![31m "%%a" does have !codec_token[%%i]! !ESC![0m
+					if [%log_sub_codec_output%]==[1] (
+						echo "%%a" - "!codec_token[%%i]!" >> %root_path:"=%!codec_token[%%i]!.txt
+					)
+					if [%remove_subtitles_codecs%]==[1] (
+						"%mkvtoolnix_path:"=%mkvmerge.exe" -o "%directory_path_name:"=%.tmp" --no-subtitles "%plex_folder:"=%"
+						if errorlevel 1 (
+							echo Warnings/errors generated during removing subtitles
+						) else (
+							echo Successfully removed subtitles
+						)
+						move /y "%directory_path_name:"=%.tmp" "%plex_folder:"=%"
+					)
+				)
+			)
+		)
+		setLocal EnableDelayedExpansion
+
+goto :end_script
+
+:isdir
 
 set "comd=aws iam create-group %video_formats:"=%"
 for /F "tokens=3*" %%p in ("%comd%") do set "tokens=%%q"
@@ -64,20 +167,33 @@ for %%a in (%tokens:-=%) do (
 for /l %%i in (1,1,%n%) do (
 	echo Enumerating all !token[%%i]!s under "%plex_folder:"=%"
 	for /r "%plex_folder:"=%" %%a in (*!token[%%i]!) do (
-		rem echo filename %%~dpna fileextension %%~xa
-		setlocal DisableDelayedExpansion
-		"%mkvtoolnix_path:"=%mkvmerge.exe" -o "%%~dpna.tmp" --no-subtitles "%%a"
-		if errorlevel 1 (
-			echo Warnings/errors generated during removing subtitles
-		) else (
-			echo Successfully removed subtitles
+		for /l %%i in (1,1,%codec_n%) do (
+			echo Enumerating all !codec_token[%%i]! under "%%a"
+			rem echo filename %%~dpna fileextension %%~xa
+			for /f %%b in ('%root_path:"=%win-x64\Tools\FfMpeg\bin\ffmpeg.exe -i "%%a" 2^>^&1 ^| find /c /i "Subtitle: !codec_token[%%i]!"') do (
+				if [%%b]==[0] (
+					echo !ESC![32m "%%a" does not have !codec_token[%%i]! !ESC![0m
+				) else (
+					echo !ESC![31m "%%a" does have !codec_token[%%i]! !ESC![0m
+					if [%log_sub_codec_output%]==[1] (
+						echo "%%a" - "!codec_token[%%i]!" >> %root_path:"=%!codec_token[%%i]!.txt
+					)
+					if [%remove_subtitles_codecs%]==[1] (
+						"%mkvtoolnix_path:"=%mkvmerge.exe" -o "%%~dpna.tmp" --no-subtitles "%%a"
+						if errorlevel 1 (
+							echo Warnings/errors generated during removing subtitles
+						) else (
+							echo Successfully removed subtitles
+						)
+						move /y "%%~dpna.tmp" "%%a"
+					)
+				)
+			)
 		)
-		move /y "%%~dpna.tmp" "%%a"
-		setLocal EnableDelayedExpansion
 	)
 )
 
-::End Remove Subtitle code
+::End Forced Subtitle code
 
 goto :end_script
 
@@ -194,4 +310,4 @@ if not %wait_interval% == 0 TIMEOUT /T %wait_interval%
 
 if %looping% == 1 goto :start_exe
 
-exit
+exit /b
